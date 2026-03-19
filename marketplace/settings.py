f387e3ad02273282381fa10a9910f6015aaacfb3
anjiolinanakayacontact-netizen <anjiolinanakayacontact@gmail.com>
2026-03-19 14:23:05 +0000

    Fix settings for production

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+vsqefmu#8(=5f(=d3$79c-c1u0)_zm4vm8$e^qa8$w3ea-6^p'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
