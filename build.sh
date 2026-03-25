f527335569ca264140b82726d8336674bc066c0c
anjiolinanakayacontact-netizen <anjiolinanakayacontact@gmail.com>
2026-03-25 02:55:34 +0000

    Reset admin password

--- build.sh
+++ build.sh
@@ -15,3 +15,8 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 EOF
 >>>>>>> 17c4b19adfb3a7de101fac88df4c78870c832492
+
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shells
