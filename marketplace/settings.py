eb716508af91620eff7c3e6032fb5a749d1a3f3d
anjiolinanakayacontact-netizen <anjiolinanakayacontact@gmail.com>
2026-04-03 17:16:54 +0000

    Remove footer links from homepage

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -42,7 +42,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
